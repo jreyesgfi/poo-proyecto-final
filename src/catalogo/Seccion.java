4afc2d1cf6853527f26e8e52bb313f07f66180fb
Héctor Torres <hector.torres23@estudiantes.uva.es>
2024-12-11 12:57:31 +0100

    feat (catálogo): 📦 Add Seccion class

--- src/catalogo/Seccion.java
+++ src/catalogo/Seccion.java
@@ -1,4 +1,56 @@
 package catalogo;
 
+/**
+ * La clase Seccion representa una sección con un nombre y una descripción.
+ */
 public class Seccion {
+    private String nombre;
+    private String descripcion;
+
+    /**
+     * Constructor para inicializar un objeto Seccion.
+     *
+     * @param nombre El nombre de la sección.
+     * @param descripcion La descripción de la sección.
+     */
+    public Seccion(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * Obtiene el nombre de la sección.
+     *
+     * @return El nombre de la sección.
+     */
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    /**
+     * Obtiene la descripción de la sección.
+     *
+     * @return La descripción de la sección.
+     */
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    /**
+     * Establece el nombre de la sección.
+     *
+     * @param nombre El nombre de la sección.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Establece la descripción de la sección.
+     *
+     * @param descripcion La descripción de la sección.
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
