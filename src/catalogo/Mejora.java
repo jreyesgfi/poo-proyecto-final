f3cf048de52ea0822814bc208b03f90bd1a570b6
Héctor Torres <hector.torres23@estudiantes.uva.es>
2024-12-11 12:59:21 +0100

    feat (catálogo): 📦 Add Mejora class

--- src/catalogo/Mejora.java
+++ src/catalogo/Mejora.java
@@ -1,4 +1,38 @@
 package catalogo;
 
+/**
+ * La clase Mejora representa una mejora con un nombre y un porcentaje de incremento.
+ */
 public class Mejora {
+    private String nombre;
+    private float porcentajeIncremento;
+
+    /**
+     * Constructor para inicializar un objeto Mejora.
+     *
+     * @param nombre El nombre de la mejora.
+     * @param porcentajeIncremento El porcentaje de incremento del precio base.
+     */
+    public Mejora(String nombre, float porcentajeIncremento) {
+        this.nombre = nombre;
+        this.porcentajeIncremento = porcentajeIncremento;
+    }
+
+    /**
+     * Obtiene el nombre de la mejora.
+     *
+     * @return El nombre de la mejora.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Obtiene el porcentaje de incremento de la mejora.
+     *
+     * @return El porcentaje de incremento.
+     */
+    public float getPorcentajeIncremento() {
+        return porcentajeIncremento;
+    }
 }
